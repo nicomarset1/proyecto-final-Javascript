3b19496bb624b0d874aaed86dba4d697f99c69bb
nicomarset <nicolasmarsetg@gmail.com>
2023-08-17 20:53:09 -0300

    final

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
-// Iniciamos la variable carrito
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-/* -------------------------------- FUNCIONES ------------------------------- */
+// FUNCIÓN PARA MOSTRAR LOS PRODUCTOS
 const mostrarProductos = (productos) => {
-
 	const contenedorProductos = document.querySelector(".product-list");
-	
 	contenedorProductos.innerHTML = "";
-
 	productos.forEach((producto) => {
 		const li = document.createElement("li");
 		li.innerHTML = `
@@ -17,91 +13,74 @@ const mostrarProductos = (productos) => {
     <p class="product-price">$${producto.precio}</p>
     <button id="agregar-${producto.id}" class="add-to-cart">Agregar al carrito</button>
     `;
-	
 		contenedorProductos.appendChild(li);
-	
 		const boton = document.getElementById(`agregar-${producto.id}`);
-	
 		boton.addEventListener("click", () => {
-			agregarAlCarrito(producto.id);
+			agregarAlCarrito(productos, producto.id);
 		});
 	});
 };
 
-//AGREGAR EL PRODUCTO AL CARRITO
-const agregarAlCarrito = (id) => {
-
+// FUNCIÓN PARA AGREGAR EL PRODUCTO AL CARRITO
+const agregarAlCarrito = (productos, id) => {
 	if (!carrito.some((producto) => producto.id === id)) {
-	
 		const producto = productos.find((producto) => producto.id === id);
-        
 		carrito.push({ ...producto, cantidad: 1 });
- //       alert("Se agrego un producto al carrito!")
-    Swal.fire({
-        icon: 'success',
-        title: 'Producto añadido!',
-        text: 'Producto añadido al carrito con exito!',
-    })
+		Swal.fire({
+			icon: 'success',
+			title: 'Producto añadido!',
+			text: 'Producto añadido al carrito con exito!',
+		})
 	} else {
-
 		const producto = carrito.find((producto) => producto.id === id);
 		producto.cantidad++;
 	}
-
 	localStorage.setItem("carrito", JSON.stringify(carrito));
-
 	mostrarCarrito();
 };
 
 const mostrarCarrito = () => {
-
 	const contenedorCarrito = document.querySelector(".carrito");
-
 	contenedorCarrito.innerHTML = "";
-
-if (carrito.length > 0) {
-	
-	const productsCart = document.createElement("ul");
-	productsCart.classList.add("productsCart");
-	contenedorCarrito.appendChild(productsCart);
-
-	const contenedorTotal = document.createElement("p");
-	actualizarTotal(contenedorTotal);
-	contenedorCarrito.appendChild(contenedorTotal);
-
-	carrito.forEach((producto) => {
-		const li = document.createElement("li");
-		li.innerHTML = `
-		<img src="${producto.imagen}" alt="${producto.nombre}" />
-		<div class="productContent">
-			<h3>${producto.nombre}</h3>
-			<p class="product-description">${producto.descripcion}</p>
-			<p class="product-price">$${producto.precio}</p>
-			<div class="counter">
-			<button id="decrementar-${producto.id}" class="button">-</button>
-			<span class="product-price">${producto.cantidad}u.</span>
-			<button id="incrementar-${producto.id}" class="button">+</button>
+	if (carrito.length > 0) {
+		const productsCart = document.createElement("ul");
+		productsCart.classList.add("productsCart");
+		contenedorCarrito.appendChild(productsCart);
+		const contenedorTotal = document.createElement("p");
+		actualizarTotal(contenedorTotal);
+		contenedorCarrito.appendChild(contenedorTotal);
+		carrito.forEach((producto) => {
+			const li = document.createElement("li");
+			li.innerHTML = `
+			<img src="${producto.imagen}" alt="${producto.nombre}" />
+			<div class="productContent">
+				<h3>${producto.nombre}</h3>
+				<p class="product-description">${producto.descripcion}</p>
+				<p class="product-price">$${producto.precio}</p>
+				<div class="counter">
+				<button id="decrementar-${producto.id}" class="button">-</button>
+				<span class="product-price">${producto.cantidad}u.</span>
+				<button id="incrementar-${producto.id}" class="button">+</button>
+				</div>
 			</div>
-		</div>
-		<button id="eliminar-${producto.id}" class="remove">Eliminar</button>
-	`;
+			<button id="eliminar-${producto.id}" class="remove">Eliminar</button>
+		`;
+			productsCart.appendChild(li);
+			const boton = document.getElementById(`eliminar-${producto.id}`);
+			boton.addEventListener("click", () => {
+				eliminarProducto(producto.id);
+			});
 
-		productsCart.appendChild(li);
-const boton = document.getElementById(`eliminar-${producto.id}`);
-boton.addEventListener("click", () => {
-eliminarProducto(producto.id);
+			const decrementar = document.getElementById(`decrementar-${producto.id}`);
+			decrementar.addEventListener("click", () => {
+				decrementarProducto(producto.id);
+			});
+
+			const incrementar = document.getElementById(`incrementar-${producto.id}`);
+			incrementar.addEventListener("click", () => {
+				incrementarProducto(producto.id);
+			});
 		});
-
-const decrementar = document.getElementById(`decrementar-${producto.id}`);
-decrementar.addEventListener("click", () => {
-decrementarProducto(producto.id);
-});
-
-const incrementar = document.getElementById(`incrementar-${producto.id}`);
-incrementar.addEventListener("click", () => {
-	incrementarProducto(producto.id);
-    });
-});
 	} else {
 		contenedorCarrito.innerHTML = '<p class="empty">No hay productos</p>';
 	}
@@ -136,6 +115,9 @@ const actualizarTotal = (contenedor) => {
 	contenedor.textContent = `Total: $${total}`;
 };
 
-
-mostrarProductos(productos);
-mostrarCarrito();
+fetch("./js/mouse.json")
+	.then((response) => response.json())
+	.then((productos) => {
+		mostrarProductos(productos);
+		mostrarCarrito();
+	});
